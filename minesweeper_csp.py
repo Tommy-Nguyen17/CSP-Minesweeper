737f7b7723a225389e60d15e227dbbefc583fcdf
TommyN17 <166442927+Tommy-Nguyen17@users.noreply.github.com>
2025-11-12 09:47:29 -0500

    slight update to init repo

--- minesweeper_csp.py
+++ minesweeper_csp.py
@@ -1,2 +1,19 @@
 # Project by Paige Althoff, Andrew Elko & Tommy Nguyen
 
+# Import necessary to use the Z3 Library
+import z3
+
+"""
+
+Modelling a 5x5 Minesweeper board:
+
+instance = ((? ? 1 ? 1 )
+            (1 ? ? ? ? )
+            (? ? 0 ? ? )
+            (? ? ? ? 1 )
+            (2 ? 1 ? ? ))
+
+"""
+
+solver = z3.Solver()
+
