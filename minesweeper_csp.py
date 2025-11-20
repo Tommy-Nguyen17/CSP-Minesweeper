f40751b3411ec54d8ef1c5b469319ca8d52f82f9
Paige Althoff <palthoff20@gmail.com>
2025-11-19 22:21:54 -0500

    MAYBEEEE

--- minesweeper_csp.py
+++ minesweeper_csp.py
@@ -35,14 +35,18 @@ solver = z3.Solver()
 
 hasMine = []
 numberSpace = []
+# For printing the board
+user_input = []
 
 # Need number space info somehow
 for row in range(0, 5):
     hasMine.append([])
     numberSpace.append([])
+    user_input.append([])
     for col in range (0, 5):
         hasMine[row].append(z3.Bool("*" + str(row) + str(col)))
         numberSpace[row].append(z3.Int(str(row) + str(col)))
+        user_input[row].append(None)
 
 # Background knowledge about what the numbers mean.
 for row in range(0, 5):
@@ -66,28 +70,51 @@ print("On this board, _ marks a square definitely doesn't contain a mine")
 print("\tand * marks a square that definitely contains a mine,")
 print("\twhile a number tells you how many mines are adjacent to that square")
 
-# Making unknown squares negative so the numberSpace = notMine constraint works
-for row in range(0, 5):
-    for col in range(0, 5):
-        solver.add(numberSpace[row][col] == -1)
-
 answer = "Ford the River"
 while answer != "Q":
-    # Print out stuff we know about the adjacent squares
-    # TODO
+    # Current board
+    print("\nCurrent Board:")
+    for row in range(0, 5):
+        for col in range(0, 5):
+            if user_input[row][col] is not None:
+                print(user_input[row][col], end=" ")
+            elif provable(solver, hasMine[row][col]):
+                print("*", end=" ")
+            elif provable(solver, z3.Not(hasMine[row][col])):
+                print("_", end=" ")
+            # elif provable(solver, ): print("", end="")
+            else:
+                print("?", end=" ")
+        print()
 
     print("You are exploring a grid of rows 0-4 and columns 0-4.")
-    print("Please type in one of the following:")
+    print("Please enter your board by typing in one of the following:")
     # Type in info about the number placements
-    print("\t2 0 1 if there are 2 mines adjacent to row 0 column 1")
-    print("\tQ if you wish to exit because you won / died / got bored")
+    print("  2 0 1 if there are 2 mines adjacent to row 0 column 1")
+    print("  Q if you have finished inputting the board and wish to see your solution or give up")
 
     # Find out what happened at this time step.
     answer = input("Please enter your board as (number of mines adjacent) (row) (column): ")
     # Adding user input into solve, overwriting the  numberSpaces
     if len(answer) == 5 and answer[0] in ["0", "1", "2", "3", "4", "5", "6", "7", "8"] and answer[1] == " " and answer [3] == " " and answer[2] in ["0", "1", "2", "3", "4"] and answer[4] in ["0", "1", "2", "3", "4"]:
         solver.add(numberSpace[int(answer[2])][int(answer[4])] == int(answer[0]))
+        user_input[int(answer[2])][int(answer[4])] = int(answer[0])
     elif answer != "Q":
         print("Your response was not understood.")
+    print()
+
+# Print out final board
+print("\nFinal Board:")
+for row in range(0, 5):
+    for col in range(0, 5):
+        if user_input[row][col] is not None:
+            print(user_input[row][col], end=" ")
+        elif provable(solver, hasMine[row][col]):
+            print("*", end=" ")
+        elif provable(solver, z3.Not(hasMine[row][col])):
+            print("_", end=" ")
+        # elif provable(solver, ): print("", end="")
+        else:
+            print("?", end=" ")
     print()
 
